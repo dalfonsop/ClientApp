55cc1c6c2881e4ca39a82278442e4c65f18b2d20
dalfonsop <dalfonsop94@gmail.com>
2022-11-19 11:44:11 -0500

    remove firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,15 +14,3 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDEU-aaRcliNgpd6_9yaBGcvn1AV6wZqM4",
-  authDomain: "dev-laprimeramujer.firebaseapp.com",
-  databaseURL: "https://dev-laprimeramujer-default-rtdb.firebaseio.com",
-  projectId: "dev-laprimeramujer",
-  storageBucket: "dev-laprimeramujer.appspot.com",
-  messagingSenderId: "278355631358",
-  appId: "1:278355631358:web:d82d57655cebb391df59af",
-  measurementId: "G-C7Z3SZD3B2"
-};
