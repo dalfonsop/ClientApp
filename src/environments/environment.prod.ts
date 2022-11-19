155724f33749e238d71dcd3582fc80102b21b8cd
dalfonsop <dalfonsop94@gmail.com>
2022-11-19 11:57:31 -0500

    hide firebaseconfig

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,3 @@
 export const environment = {
   production: true
 };
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDEU-aaRcliNgpd6_9yaBGcvn1AV6wZqM4",
-  authDomain: "dev-laprimeramujer.firebaseapp.com",
-  databaseURL: "https://dev-laprimeramujer-default-rtdb.firebaseio.com",
-  projectId: "dev-laprimeramujer",
-  storageBucket: "dev-laprimeramujer.appspot.com",
-  messagingSenderId: "278355631358",
-  appId: "1:278355631358:web:d82d57655cebb391df59af",
-  measurementId: "G-C7Z3SZD3B2"
-};
